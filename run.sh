ffce9bca609789309aa08bdc118a57d57b4a8b74
Gamiz63 <59408981+Gamiz63@users.noreply.github.com>
2020-08-11 06:20:40 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,16 @@ git clone https://github.com/AdrMXR/KitHack.git
 git clone https://github.com/FluxionNetwork/fluxion.git
 mv -v fluxion flux
 mv -v KitHack tools
+mv start1.sh flux
+cd flux
+mv -v start1.sh start.sh
+chmod +x start.sh
+cd ..
+mv start2.sh tools
+cd tools
+mv -v start2.sh start.sh
+chmod +x start.sh
+cd ..
 mv -f * ~/
 cd ..
 mv -v -f 1.bashrc .bashrc
@@ -13,4 +23,14 @@ chmod +x RW.sh
 reset
 rm -r ParroTools
 rm -r run.sh
+clear
+cd flux
+chmod +x fluxion.sh
+./fluxion -i
+cd ..
+cd tools
+chmod +x install.sh
+./install.sh
+cd ..
+reset
 clear
