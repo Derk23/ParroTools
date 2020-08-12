f33e3141dd32e1bc8911fa85d523e51dbafdf1bb
Gamiz63 <59408981+Gamiz63@users.noreply.github.com>
2020-08-12 06:27:23 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,16 +7,25 @@ apt update && apt full-upgrade
 apt install python && apt install python3 && apt install python-pip && apt install python3-pip && apt install wget && apt install php-curl && apt install php-xml
 git clone https://github.com/FluxionNetwork/fluxion.git
 git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+git clone https://github.com/4w4k3/BeeLogger.git
 mv -v fluxion flux
 mv -v RED_HAWK web1
+mv -v BeeLogger keylo1
 mv start1.sh flux
 cd flux
 mv -v start1.sh start.sh
+chmod +x fluxion.sh
 chmod +x start.sh
 cd ..
 mv start2.sh web1
 cd web1
 mv -v start2.sh start.sh
+chmod +x start.sh
+cd ..
+mv start3.sh keylo1
+cd keylo1
+mv -v start3.sh start.sh
+chmod +x install.sh
 chmod +x start.sh
 cd ..
 mv -f * ~/
@@ -31,6 +40,10 @@ clear
 cd flux
 chmod +x fluxion.sh
 ./fluxion.sh -i
+cd ..
+cd keylo1
+./install.sh
+cd ..
 apt update && apt full-upgrade
 apt update && apt full-upgrade
 apt update && apt full-upgrade
