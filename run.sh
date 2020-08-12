7db6f4dd62e0ec81af09009cabcb8f7a2898ce12
Gamiz63 <59408981+Gamiz63@users.noreply.github.com>
2020-08-12 01:39:06 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,19 @@ apt update && apt full-upgrade
 apt update && apt full-upgrade
 apt update && apt full-upgrade
 apt update && apt full-upgrade
-apt install python && apt install python3 && apt install python-pip && apt install python3-pip && apt install wget
+apt install python && apt install python3 && apt install python-pip && apt install python3-pip && apt install wget && apt install php-curl && apt install php-xml
 git clone https://github.com/FluxionNetwork/fluxion.git
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
 mv -v fluxion flux
+mv -v RED_HAWK web1
 mv start1.sh flux
 cd flux
 mv -v start1.sh start.sh
+chmod +x start.sh
+cd ..
+mv start2 web1
+cd web1
+mv -v start2.sh start.sh
 chmod +x start.sh
 cd ..
 mv -f * ~/
