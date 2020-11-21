1a343d9cb7c9522dbf6e3843cd408dd1b03df743
Gamiz63 <59408981+Gamiz63@users.noreply.github.com>
2020-11-20 20:05:26 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,8 +22,10 @@ apt install php-curl
 apt install php-xml
 git clone https://github.com/FluxionNetwork/fluxion.git
 git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+git clone https://github.com/DarkSecDevelopers/HiddenEye-Legacy.git
 mv -v fluxion flux
 mv -v RED_HAWK web1
+mv -v HiddenEye-Legacy phish1
 mv start1.sh flux
 cd flux
 mv -v start1.sh start.sh
@@ -33,6 +35,11 @@ cd ..
 mv start2.sh web1
 cd web1
 mv -v start2.sh start.sh
+chmod +x start.sh
+cd ..
+mv start3.sh phish1
+cd phish1
+mv -v start3.sh start.sh
 chmod +x start.sh
 cd ..
 mv -f * ~/
@@ -46,6 +53,9 @@ rm -r run.sh
 clear
 cd flux
 ./fluxion.sh -i
+cd ..
+cd phish1
+pip3 install -r requirements.txt
 cd ..
 apt update && apt full-upgrade
 apt update && apt full-upgrade
